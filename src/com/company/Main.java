613236089c753e2231cadd9f04fef86461450dd8
akhsa234 <saremi.bah2000@gmail.com>
2025-08-08 16:24:31 +0330

    first commit StringReverse class

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String str ="abscndjhyu" ;
+    String str2 = "";
+    String str4= null;
+    StringReverse reverse =new StringReverse();
+    String result =reverse.reverse(str4);
+        System.out.println(result);
+    }
+}
